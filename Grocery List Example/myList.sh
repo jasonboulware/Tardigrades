1baa19a2e95922168ce1d8281f63e7c31c9140b1
Austin Hollis <40678619+hollisas@users.noreply.github.com>
2018-09-24 15:11:09 -0400

    Update myList.sh
    Auto launches the created html file in firefox.

--- Grocery List Example/myList.sh
+++ Grocery List Example/myList.sh
@@ -2,7 +2,7 @@
 
 # Tardigrades
 
-title="Directory Contents of ${PWD##*/}"
+#title="Directory Contents of ${PWD##*/}"
 
 displayContents() {
   for file in ./*; 
@@ -12,7 +12,9 @@ displayContents() {
     echo "</li>"
   done
 }
-
+write_page()
+{
+title="Directory Contents of ${PWD##*/}"
 cat <<- _EOF_
 <html>
   <head>
@@ -32,3 +34,16 @@ cat <<- _EOF_
   </body>
 </html>
 _EOF_
+}
+
+launch()
+{
+	firefox myList.html
+}
+
+####Main
+
+filename=myList.html
+write_page > $filename
+chmod 755 $filename
+launch

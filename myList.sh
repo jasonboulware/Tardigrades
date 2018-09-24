1fb55e466bec252cf36513cb95ffb41846664ac0
Austin Hollis <40678619+hollisas@users.noreply.github.com>
2018-09-24 15:15:42 -0400

    Updated file

--- myList.sh
+++ myList.sh
@@ -4,18 +4,20 @@
 # CSCI 362
 # FALL 2018
 
-# The ${PWD##*/} displays the current directory name
-title="${PWD##*/}"
-header = "Directory Contents of ${PWD##*/}"
-
 displayContents() {
-  for file in ./*; do
+  for file in ./*; 
+  do
     echo "<li>"
-    echo "$(basename "$file")" #Removes path information
+    echo "$(basename "$file")"
     echo "</li>"
   done
 }
 
+write_page()
+{
+# The ${PWD##*/} displays the current directory name
+header = "Directory Contents of ${PWD##*/}"
+title="${PWD##*/}"
 cat <<- _EOF_
 <html>
 
@@ -32,3 +34,16 @@ cat <<- _EOF_
   
 </html>
 _EOF_
+}
+
+launch()
+{
+	firefox myList.html
+}
+
+####Main
+
+filename=myList.html
+write_page > $filename
+chmod 755 $filename
+launch
